4e7e5767a865fce4547b96f0d218e346dfd13539
Andy Nicholson <andrew@anicholson.net>
2018-10-19 10:48:30 +1100

    Flesh out example a bit

--- src/example.cr
+++ src/example.cr
@@ -9,16 +9,17 @@ OptionParser.parse! do |parser|
   parser.banner = "Example gopher.cr server"
 
   parser.on("-p LISTEN_PORT", "--port=LISTEN_PORT", "Port to listen on (default: 70)") { |port| config.listen_port = port.to_u16 }
-  parser.on("-h PUBLIC_HOST", "--public-host=PUBLIC_HOST", "FQ name of the server (default: localhost)") { |hostname| config.public_host = hostname }
-  parser.on("-pp PUBLIC_PORT", "--public-port=PUBLIC_PORT", "Public port to pass to clients. (Useful if server lives behind a gateway)") { |public_port| config.public_port = public_port.to_u16 }
+  parser.on("-H PUBLIC_HOST", "--public-host=PUBLIC_HOST", "FQ name of the server (default: localhost)") { |hostname| config.public_host = hostname }
+  parser.on("-o PUBLIC_PORT", "--public-port=PUBLIC_PORT", "Public port to pass to clients. (Useful if server lives behind a gateway)") { |public_port| config.public_port = public_port.to_u16 }
+  parser.on("-h", "--help", "Show this help") { puts parser; exit 0 }
 end
 
 server = G::Server.new config
 
 about = G::SelectorResolver.new("/", G::Resource.new("Hello from gopherland"), G::MenuEntryType::TextFile)
-example_directory = G::DirectoryResolver.new(File.dirname(__FILE__) + "/../spec/resources/example_directory")
+example_directory = G::DirectoryResolver.new(File.dirname(__FILE__) + "/../spec/resources/example_directory", "", config)
 
-resolver = Gopher::MultiResolver.new(host: server.host, port: server.port)
+resolver = Gopher::MultiResolver.new("", config)
 resolver.add_resolver("hello", about)
 resolver.add_resolver("stuff", example_directory)
 
