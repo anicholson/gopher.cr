b679706ea72e90f2b96713442b5b75a4e4661fa2
Andy Nicholson <andrew@anicholson.net>
2018-10-24 10:16:33 +1100

    Don't use ECR to render the default error screen.
    Closes #8

--- src/gopher/default_error_renderer.cr
+++ src/gopher/default_error_renderer.cr
@@ -1,13 +1,25 @@
-require "ecr"
-require "ecr/macros"
-
 module Gopher
   class DefaultErrorRenderer
     def initialize(@message : String)
     end
 
     def to_s(io)
-      ECR.embed("src/gopher/default_error_message.ecr", io)
+      io << "i  ______ _____  _____   ____  _____         __  " << "\n"
+      io << "i |  ____|  __ \|  __ \ / __ \|  __ \   _   / /  " << "\n"
+      io << "i | |__  | |__) | |__) | |  | | |__) | (_) | |   " << "\n"
+      io << "i |  __| |  _  /|  _  /| |  | |  _  /      | |   " << "\n"
+      io << "i | |____| | \ \| | \ \| |__| | | \ \   _  | |   " << "\n"
+      io << "i |______|_|  \_|_|  \_\\____/|_|  \_\ (_) | |   " << "\n"
+      io << "i                                           \_\  " << "\n"
+      io << "i                                                " << "\n"
+      io << "i                                                " << "\n"
+      io << "iOh dear, something went wrong with your request." << "\n"
+      io << "i                                                " << "\n"
+      io << "iHere's what we know:                            " << "\n"
+      io << "i                                                " << "\n"
+      io << "3#{@message}                                     " << "\n"
+      io << "i                                                " << "\n"
+      io << "iPlease let the site administrator know.         " << "\n"
     end
   end
 end
