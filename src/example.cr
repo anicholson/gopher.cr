3bf16b8a48d3697156bdb8f378a6c3d1dc0dfacc
Andy Nicholson <andrew@anicholson.net>
2019-01-08 14:41:16 +1100

    Add tracing option to the example app

--- src/example.cr
+++ src/example.cr
@@ -4,6 +4,8 @@ require "option_parser"
 # :nodoc:
 alias G = Gopher
 
+trace_level = Trace.default_level
+
 config = G::Config.new listen_port: 70_u16, listen_host: "0.0.0.0", public_port: 70_u16, public_host: "0.0.0.0"
 
 OptionParser.parse! do |parser|
@@ -12,8 +14,15 @@ OptionParser.parse! do |parser|
   parser.on("-p LISTEN_PORT", "--port=LISTEN_PORT", "Port to listen on (default: 70)") { |port| config.listen_port = port.to_u16 }
   parser.on("-H PUBLIC_HOST", "--public-host=PUBLIC_HOST", "FQ name of the server (default: localhost)") { |hostname| config.public_host = hostname }
   parser.on("-o PUBLIC_PORT", "--public-port=PUBLIC_PORT", "Public port to pass to clients. (Useful if server lives behind a gateway)") { |public_port| config.public_port = public_port.to_u16 }
+
+  {% if flag?(:trace) %}
+    parser.on("-l LEVEL", "--log-level=LEVEL", "Set logging level debug|info|warn|error (default: error)") { |level| trace_level = Trace::Level.from_value(level) }
+  {% end %}
+
   parser.on("-h", "--help", "Show this help") { puts parser; exit 0 }
 end
+
+Trace.default_level = trace_level
 
 server = G::Server.new config
 
